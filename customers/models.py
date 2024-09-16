34742cc857b701bafd754c6d6026d3423594578c
rishanis <rishanis@outlook.com>
2024-09-16 07:54:39 +0400

    api_view

--- customers/models.py
+++ customers/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=200)
     industy = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,23 @@
 from customers.models import Customer
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from customers.serializers import CustomerSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-def Customers(request):
-    #invoke seralizer and return client
+def customers(request):
     data = Customer.objects.all()
     serializer = CustomerSerializer(data, many=True)
-    return JsonResponse({'customer': serializer.data})
+    return JsonResponse({'customers': serializer.data})
+
+
+@api_view(['GET', 'POST', 'DELETE'])
+def customer(request, id):
+    try:
+        data = Customer.objects.get(pk=id)
+    except Customer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = CustomerSerializer(data)
+    return Response({'customer': serializer.data}, status=status.HTTP_200_OK)
+
+
