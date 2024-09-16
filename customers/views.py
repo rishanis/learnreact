9de4979de4f4660ab5269052efa5509799c53848
rishanis <rishanis@outlook.com>
2024-09-16 07:55:11 +0400

    api_view_2

--- customers/views.py
+++ customers/views.py
@@ -5,10 +5,21 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def customers(request):
-    data = Customer.objects.all()
-    serializer = CustomerSerializer(data, many=True)
-    return JsonResponse({'customers': serializer.data})
+    if request.method == 'GET':
+        data = Customer.objects.all()
+        serializer = CustomerSerializer(data, many=True)
+        return Response({'customers': serializer.data})
+    
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'customer': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
 @api_view(['GET', 'POST', 'DELETE'])
@@ -16,8 +27,23 @@ def customer(request, id):
     try:
         data = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = CustomerSerializer(data)
-    return Response({'customer': serializer.data}, status=status.HTTP_200_OK)
+        return Response(data, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = CustomerSerializer(data)
+        return Response({'customer': serializer.data})
+    
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'cusotmer': serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
